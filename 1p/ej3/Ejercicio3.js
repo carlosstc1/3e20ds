b24a42dc00d811042e4abc6320092119aad2ef71
carlosstc1 <ctene@ucol.mx>
2020-11-02 23:44:10 -0600

    commit main

---
+++ 1p/ej3/Ejercicio3.js
@@ -0,0 +1,14 @@
+//Imprimir series de n=10 números según corresponda
+
+//usar iteraciones de k={1,n}
+
+//no leer datos, ie usar constantes x=3
+
+//fibonacci
+
+const n = 10;
+var s = 1;
+for(var i=0; i<n; i++){
+console.log(s);
+s = s + 2;
+}
