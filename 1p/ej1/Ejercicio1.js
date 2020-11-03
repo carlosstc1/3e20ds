1a28771ddf7ccc37749906fda1aad6231b650c36
carlosstc1 <ctene@ucol.mx>
2020-11-02 23:24:47 -0600

    commit main

---
+++ 1p/ej1/Ejercicio1.js
@@ -0,0 +1,9 @@
+//Imprimir series de n=10 números según corresponda
+//usar iteraciones de k={1,n}
+//no leer datos, ie usar constantes x=3
+const n = 10;
+var s = 1;
+for(var i=0; i<n; i++){
+console.log(s);
+s = s + 2;
+}
