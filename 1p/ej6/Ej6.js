553be63e6d58c31a128ea3830834ae77db50a268
carlosstc1 <ctene@ucol.mx>
2020-11-03 13:49:51 -0600

    commit

--- 1p/ej6/Ej6.js
+++
@@ -1,9 +0,0 @@
-//Imprimir series de n=10 números según corresponda
-//usar iteraciones de k={1,n}
-//no leer datos, ie usar constantes x=3
-const n = 10;
-const k = 1;
-for (var i = 0; i < n; i++) {
-    var aleatorio = Math.round(Math.random()*n);
-    console.log(aleatorio);
- }
