064d791e24fe39e59c122cc93ed23f003c0c7fd4
carlosstc1 <ctene@ucol.mx>
2020-11-10 16:32:25 -0600

    Terminado

--- 1p/Series/Ejercicio2.js
+++ 1p/Series/Ejercicio2.js
@@ -2,4 +2,18 @@
 //usar iteraciones de k={1,n}
 //no leer datos, ie usar constantes x=3
 //Serie de números impares alternando signo.
-d
+let val1=1;                                 
+for(let s=0;;s++){                     
+    if(s%2==1){                       
+        if(val1%2==1){                  
+            console.log(s*-1);
+        }
+        else if (val1%2==0){           
+            console.log(s);
+        }
+    val1++;                         
+    }
+    if(val1>10){
+        break;                
+    }
+}
