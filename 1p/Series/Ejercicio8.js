cf18dcef0cd2d44de3462c8a8d9745eeed9ae03a
carlosstc1 <ctene@ucol.mx>
2020-11-10 17:52:50 -0600

    Serie Potencia "x" a la "y"

--- 1p/Series/Ejercicio8.js
+++ 1p/Series/Ejercicio8.js
@@ -1,5 +1,8 @@
 //Potencia x a la y
-var x = 3;
-var y = 6;
+var x = 2;
+var y = 1;
+for(var i = 1; i<= 10; i++){
 var p = Math.pow(x, y);
-console.log(p);
+console.log('El '+x+' elevado a '+y+' es igual a: ' +p);
+y ++;
+}
