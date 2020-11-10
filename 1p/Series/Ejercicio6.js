57062a5ee237f6fa1c7a15e528997378830c5897
carlosstc1 <ctene@ucol.mx>
2020-11-10 17:25:07 -0600

    Terminado

--- 1p/Series/Ejercicio6.js
+++ 1p/Series/Ejercicio6.js
@@ -1,7 +1,9 @@
-let val1 = 1;
-let val2 = 0;
+let L0 = 2;
+let L1 = 1;
+let Ln = 1;
 for(var i = 1; i<=10; i++){
-    val1 = i;
-    val2 = (val1*(val1 + 1))/2;
-    console.log(val2);
+    console.log(L0);
+    Ln = L0 + L1;
+    L0 = L1;
+    L1 = Ln 
 }
