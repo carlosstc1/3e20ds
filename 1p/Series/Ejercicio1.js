5f6e889645c6401c243e5f6baf47d1ef5bff1bb9
carlosstc1 <ctene@ucol.mx>
2020-11-10 17:42:56 -0600

    Serie 1, 3, 5....

--- 1p/Series/Ejercicio1.js
+++ 1p/Series/Ejercicio1.js
@@ -1,6 +1,4 @@
-//Imprimir series de n=10 números según corresponda
-//usar iteraciones de k={1,n}
-//no leer datos, ie usar constantes x=3
+//Serie 1, 3, 5...
 const n = 10;
 var s = 1;
 for(var i=0; i<n; i++){
