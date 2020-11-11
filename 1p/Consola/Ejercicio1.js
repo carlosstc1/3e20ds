6cb84fc659e1d7259603d455cd54dc89e75ffc0c
carlosstc1 <ctene@ucol.mx>
2020-11-10 23:50:10 -0600

    Aleatorios (min=1, max=(Dato de consola))

--- 1p/Consola/Ejercicio1.js
+++ 1p/Consola/Ejercicio1.js
@@ -1,1 +1,16 @@
 
+//aleatorios (min=1, max=(Dato de consola))
+var n;
+const k = 1;
+console.log('Ingrese el número máximo para los números aleatorios: ');
+process.stdin.on('data', function(data){
+n = data.toString();
+
+for (var i = 0; i < 10; i++) {
+    var aleatorio = Math.round(Math.random()*n);
+    console.log(aleatorio);
+ }
+//console.log(k);
+process.exit();
+
+})
