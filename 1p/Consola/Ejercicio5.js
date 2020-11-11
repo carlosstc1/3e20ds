34fafcacd722119d5243f831cdccfef569200ac8
carlosstc1 <ctene@ucol.mx>
2020-11-11 17:51:15 -0600

    Ln (1+x) = x - x^2/2 + x^3/3 - ... +

--- 1p/Consola/Ejercicio5.js
+++ 1p/Consola/Ejercicio5.js
@@ -1,20 +1,17 @@
-var x;
-let z = 1;
-let p = [];
+let x;
+let ln = [];
 
-console.log('Coloque el tamaño de la serie que desea: ');
+console.log('Coloque la cantidad de elementos que desea: ');
 process.stdin.on('data', function(data){
     x = data.toString();
-
     for(var i = 0; i < x; i++){
-        p[i] = Math.log(z);
-        z++
-
+        if(i%2==0){
+            console.log('['+(i+1)+'] - x^'+(i+1)+'/'+(i+1));
+        }
+        else{
+            console.log('['+(i+1)+'] + x^'+(i+1)+'/'+(i+1));
+        }
+        
     }
-    for(var t = 0; t < x; t++){
-    console.log('El "Ln" de '+(t+1)+' es: ' +p[t]);
-}
     process.exit();
-
-
 })
