d54b453e94f82dc8285ef5d27693824761cc2033
carlosstc1 <ctene@ucol.mx>
2020-11-11 18:28:20 -0600

    sen x = x - x^3/3! + ... -

--- 1p/Consola/Ejercicio7.js
+++ 1p/Consola/Ejercicio7.js
@@ -4,11 +4,11 @@ var sen = 0;
 console.log('Coloque el tamaño que desea la serie: ');
 process.stdin.on('data', function(data){
     cont = data.toString();
-console.log('Coloque el valor por el que quiere empezar (x): ');
 
-x = process.stdin.on();
-for(var i = 1; i < cont; i++){
+for(var i = 1; i <= cont; i++){
     sen = Math.sin(x);
     console.log('El seno de '+x+ ' Es igual a: '+sen);
     x++;
-}})
+}
+process.exit();
+})
