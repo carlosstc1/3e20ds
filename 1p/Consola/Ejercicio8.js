1b8cc7c81b216fbf2b4008c129713eae65ababa1
carlosstc1 <ctene@ucol.mx>
2020-11-11 18:16:35 -0600

    har = 1 + 1/2 + ... + //suma harmónica

---
+++ 1p/Consola/Ejercicio8.js
@@ -0,0 +1,22 @@
+let x;
+console.log('Coloque el tamaño que desea la serie: ');
+process.stdin.on('data', function(data){
+    x = data.toString();
+
+    for(var i = 1; i <= x; i++){
+        if((i)== 1){
+            console.log('['+i+'] '+i);
+        }
+        else{
+            if(i%2==0){
+                console.log('['+i+'] +1/'+i);
+            }
+            else{
+                console.log('['+i+'] +1/'+i);
+            }
+
+
+        }
+    }
+    process.exit();
+})
