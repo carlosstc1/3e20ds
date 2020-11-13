eee1b01cb6a116a042a263d564240265bed803d9
carlosstc1 <ctene@ucol.mx>
2020-11-13 00:18:26 -0600

    Quinto Triangular

---
+++ 1p/Quinto/Ejercicio2.js
@@ -0,0 +1,9 @@
+let val1 = 1;
+let val2 = 0;
+let array = [];
+for(var i = 0; i<10; i++){
+    val2 = (val1*(val1 + 1))/2;
+    array[i] = val2;
+    val1++;
+}
+console.log("Este array contiene "+array.length+" elementos, y el quinto es: "+array[4]);
