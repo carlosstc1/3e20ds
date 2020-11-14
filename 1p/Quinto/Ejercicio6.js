709acb613555385cff370328c3f0787dff1b883b
carlosstc1 <ctene@ucol.mx>
2020-11-13 23:08:15 -0600

    Quinto Lucas

---
+++ 1p/Quinto/Ejercicio6.js
@@ -0,0 +1,12 @@
+//Quinto lucas
+let L0 = 2;
+let L1 = 1;
+let Ln = 1;
+let array = [];
+for(var i = 0; i<10; i++){
+    array[i] = L0;
+    Ln = L0 + L1;
+    L0 = L1;
+    L1 = Ln 
+}
+console.log('El array contiene '+array.length+' elementos, el quinto elemento es: ' +array[4]);
