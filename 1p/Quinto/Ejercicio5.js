363a90ca8efa994689f73ba58bf6e195b204fc3e
carlosstc1 <ctene@ucol.mx>
2020-11-13 23:01:38 -0600

    Serie Factorial

---
+++ 1p/Quinto/Ejercicio5.js
@@ -0,0 +1,11 @@
+    //Quinto Factorial.
+    var n = 5;
+    var total = 1; 
+    var array = [];
+	for (i = 1; i <= n; i ++) {
+        total = total * i;
+        array[i-1] = total
+         
+	}
+	console.log('El quinto elemento es: '+array[4]);
+
