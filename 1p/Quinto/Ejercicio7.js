2957f144d6248889cfe5daefd6150316f59eb101
carlosstc1 <ctene@ucol.mx>
2020-11-13 23:30:47 -0600

    No sirve aun

---
+++ 1p/Quinto/Ejercicio7.js
@@ -0,0 +1,10 @@
+//Quinto Renglón del Triangulo de Pascal
+const z = 11;
+const x = 10
+var array = [];
+
+for(var i = 0; i < x; i ++){
+    array[i] = Math.pow(z, (i+1))
+
+}
+console.log(array[4]);
