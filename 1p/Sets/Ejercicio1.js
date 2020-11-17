46a692c7a1f61951fda654dd2a34373b69dcf279
carlosstc1 <ctene@ucol.mx>
2020-11-17 16:27:01 -0600

    Agregar 10 nombres e imprimirlos

---
+++ 1p/Sets/Ejercicio1.js
@@ -0,0 +1,17 @@
+//Agregar 10 nombres e imprimirlos
+const mySet = new Set();
+mySet.add('Carlos');
+mySet.add('Juan');
+mySet.add('Hernán');
+mySet.add('Mario');
+mySet.add('Santiago');
+mySet.add('Antonio');
+mySet.add('Francisco');
+mySet.add('Mariano');
+mySet.add('Alan');
+mySet.add('Victor');
+console.log(mySet);
+
+// const ary = ["Carlos", "Juan", "Hernán", "Mario", "Santiago", "Antonio", "Francisco", "Mariano", "Alan", "Víctor"];
+// const mySet1 = new Set(ary);
+// console.log(mySet1);
