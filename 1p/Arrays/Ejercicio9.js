1e234294bcf9e40537ecf08d443aa283e7695428
carlosstc1 <ctene@ucol.mx>
2020-11-17 21:52:09 -0600

    Array Factorial

--- 1p/Arrays/Ejercicio9.js
+++ 1p/Arrays/Ejercicio9.js
@@ -1,11 +1,10 @@
-// Factorial array
-var n = 10;
-var num1;
-var factorial = 1;
-var arr = [];
-for (let i = 0; i<n; i++){
-    arr[i] = factorial;
-    num1 = i + 2;
-    factorial = factorial * num1;
-}
-console.log (arr);
+    //Array Factorial.
+    var n = 10;
+    var total = 1; 
+    var array = [];
+	for (i = 1; i <= n; i ++) {
+        total = total * i;
+        array[i-1] = total
+        console.log(+i+'! = '+array[i-1]);
+         
+	}
