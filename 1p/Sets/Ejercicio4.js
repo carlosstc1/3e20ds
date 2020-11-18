e2f1003f798ef1da23636d8451ae904e23560482
carlosstc1 <ctene@ucol.mx>
2020-11-18 13:52:03 -0600

    tomar tiempo e imprimir resultado para, n=10e4, 10e5, 10e6

--- 1p/Sets/Ejercicio4.js
+++ 1p/Sets/Ejercicio4.js
@@ -1,13 +1,8 @@
+//tomar tiempo e imprimir resultado para, n=10e4, 10e5, 10e6
 console.time();
-
 let set = new Set();
-
 set.add(Math.pow(10,4))
-
 set.add(Math.pow(10,5))
-
 set.add(Math.pow(10,6))
-
 console.log(set)
-
 console.timeEnd();
