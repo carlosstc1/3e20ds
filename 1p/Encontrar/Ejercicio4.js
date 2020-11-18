5572829c4344bab88b6db2b06c067928e508537a
carlosstc1 <ctene@ucol.mx>
2020-11-17 21:19:59 -0600

    Existen elementos repetidos?}

--- 1p/Encontrar/Ejercicio4.js
+++ 1p/Encontrar/Ejercicio4.js
@@ -1,13 +1,20 @@
 //Existen elementos repetidos?
-var array=["FI29","FI09","JM15","FI29"]//devuelve true
-//["FI29","FI09","JM15","FI28"]//devuelve false
+var array=[3,1,4,2,3]//devuelve true
+var num = 0;
 
 var repetido = false;
 for (var i = 0; i < array.length; i++) {
     for (var j = 0; j < array.length; j++) {
         if (array[i] == array[j] && i != j) { //revisamos que i sea diferente de j, para que no compare el mismo elemento exacto.
             repetido= true;
+            num = array[i];
          }
      }
  }
- console.log(repetido);
+ if(repetido == false){
+    console.log('No hay números repetidos');
+ }
+ else{
+    console.log('Hay un número repetido, es: '+num);
+ }
+ 
