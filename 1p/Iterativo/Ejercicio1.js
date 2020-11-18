1bec22622e4032ebc81aad4ff807106d3cf10136
carlosstc1 <ctene@ucol.mx>
2020-11-18 17:01:27 -0600

    generar 10e6 numeros aleatorios //[0, 999999]

--- 1p/Iterativo/Ejercicio1.js
+++ 1p/Iterativo/Ejercicio1.js
@@ -12,5 +12,5 @@ y++
 for(var i = 0; i < array.length; i++){
 console.log(array[i]);}
 console.timeEnd();
-//el tiempo promedio fue de 1:33:22
+//el tiempo promedio fue de 1:33:22 minutos
 //se podría mejorar haciendo ordenamiento recursivo
