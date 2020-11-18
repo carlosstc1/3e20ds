a695750ac01c0975d2f95008820831d72066248c
carlosstc1 <ctene@ucol.mx>
2020-11-18 13:16:47 -0600

    En una fiesta hay n invitados, si cada uno saluda a todos los demás, cuantos saludos ocurrieron?

---
+++ 1p/Cuantas/Ejercicio1.js
@@ -0,0 +1,32 @@
+//En una fiesta hay n invitados, si cada uno saluda a todos los demás, cuantos saludos ocurrieron?
+var invitados = 4;
+var facto = invitados;
+var resultado = facto;
+var i = facto-1;
+var mFac, nFac, sFac;
+//Función Factorial
+
+function fact(resultado, i){
+while(i>1){
+
+    resultado = resultado * i;
+    i--;
+}
+return resultado;
+}   
+    nFac = fact(resultado, i);
+    //factorial 
+    facto = 2;
+    resultado = facto;
+    i = facto-1;
+
+    mFac = fact(resultado, i)
+
+    facto = 2;
+    resultado = facto;
+    i = facto-1;
+
+    sFac = fact(resultado, i);
+
+    //Se usa la fórmula = N!/(M! *(N-M)!)
+     console.log('Si en una fiesta de ['+invitados+'] invitados, cada uno saluda a los demás. Serían un total de ['+(nFac)/(mFac*sFac)+'] saludos.');
