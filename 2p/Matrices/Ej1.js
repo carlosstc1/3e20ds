e2cf783fa730d0d6cee2e879743b88503e9ac1df
carlosstc1 <ctene@ucol.mx>
2021-01-17 09:08:16 -0600

    //Matríz de 3x3 transpuesta

---
+++ 2p/Matrices/Ej1.js
@@ -0,0 +1,29 @@
+//Matríz de 3x3 transpuesta
+let Matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+let matriz= [];
+for(let i=0; i<3; i++) {
+    matriz[i]=[]
+}
+console.log("Matríz Original")
+for (let i=0; i<Matriz.length; i++){
+
+    console.log(Matriz[i]);
+}
+class Procedimiento{
+    static trasponer(){
+        for (let i=0; i<3; i++) {
+            for (let x = 0; x < 3; x++) {
+                matriz[x][i] = Matriz[i][x];
+            }
+        }
+        console.log("Matríz Transpuesta")
+        for (let i=0; i<matriz.length; i++){
+            console.log(matriz[i]);
+        }
+    }
+}
+Procedimiento.trasponer()
