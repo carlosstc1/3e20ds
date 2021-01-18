0a32f3f2f350a8fca05ef0f05db7d3994ea284f7
carlosstc1 <ctene@ucol.mx>
2021-01-17 23:09:32 -0600

    Dance

--- 2p/Colas/Ej1.js
+++ 2p/Colas/Ej1.js
@@ -1,129 +1,73 @@
-//Dance
-  
-class Dance{
-
-    constructor(){
-
-        this.dataStore = {}
-        this.front = 0;
-        this.end = 0
-
+//Dance 
+class Cola{
+    constructor() {
+        this.fin=[];
     }
-    
-    discoverSex(names,w,m){
-
-        let men = 0
-        let woman = 0
-
-      for(let i = 1; i <= names.length; i++){
-
-          if(i%2 == 0){
-
-             m[men] = names[i];
-             men++;
-
-           }
-
-          else if (i%2 > 0){
-
-             w[woman] = names[i];
-             woman++;
-          }
-       }
+    add(element){
+        this.fin.push(element)
+        return this.fin;
     }
-
-    emp(w, m, couple){
-
-        for(let i = 0; i < 7;i++)
-        {
-            let men = Math.round(Math.random()*10);
-            let woman = Math.round(Math.random()*10);
-            
-             couple[i] =  w[i] +" le tocará bailar con: "+ m[i];
-
+    quitar(){
+        return this.fin.shift();
+    }
+    primero() {
+        return this.fin[0];
+    }
+    mostrar(){
+        return this.fin;
+    }
+    vacio(){
+        if (this.fin.length == 0) {
+            return true;
+        }
+        else {
+            return false;
         }
     }
-
-    enqueue(data){
-
-        this.dataStore[this.end] = data;
-        this.end++;
-
-    }
-
-    dequeue(){
-
-        let x;
-
-        if(this.size === 0) return null;
-
-        x = this.item[this.front];
-
-        delete this.item[this.front];
-
-        this.front++;
-
-        return x;
-
-    }
-
-    size(){return this.end - this.front}
-
-    isEmpty(){
-
-        if(this.size() === 0)return true; 
-
-        else return false 
-    }
-
-    peek(){
-
-      if(this.size === 0) return null;
-
-      return this.item[this.front];
-
-    }
-
-  
-    print(){
-
-        if(this.size === 0)return null;
-
-        let result = '';
-
-        for(let i = this.front; i < this.end; i++)
-        {
-          result += this.item[i];
-        }
-        return result
-    }
-
-    clear()
-
-    {
-        this.item = [];
-        this.front = 0;
-        this.end = 0;
-    }
-
-       
 }
 
-let names = ["Ricardo","Ana","José","Rocio","Jonny","Paola","Adolfo","Carolina","Samuel","Andrea","Sebastián","Sandy","Edson","María","Jesús"]
-let ladies = [];
-let men = [];
-let couple = [];
-let  pairing = new Dance();
-pairing.discoverSex(names,ladies,men);
-pairing.emp(ladies,men,couple);
-for(let x = 0; x < couple.length; x++) pairing.enqueue(couple[x])
+function Dancer(name, sex) {
+    this.name = name;
+    this.sex = sex;
+}
 
-console.log("Total de personas participando : ",names.length -1)
-console.log("Nombres : ", names)
+function dance(hom, muj) {
+    console.log('+--------------------------------+')
+    console.log("    Lista de Parejas de baile: ");
+    console.log('+--------------------------------+');
+    var counter = 1;
+    while (!muj.vacio() && !hom.vacio()) {
+     
+        
 
-console.log("Bailarinas : " , ladies.toString())
-console.log("Bailarines :" , men.toString())
+        person = muj.quitar();
+        person2 = hom.quitar();
+        console.log("Pareja número "+counter+ ' --> '+ person.name+ ' y ' +person2.name);
+        counter++
+       
+    }
+    console.log();
+}
+let hombre = new Cola();
+let mujer = new Cola();
 
-console.log("Cola de parejas", pairing)
+hombre.add(new Dancer("Carlos", "h"))
+hombre.add(new Dancer("Arturo", "h"))
+hombre.add(new Dancer("Enrique", "h"))
+hombre.add(new Dancer("Francisco", "h"))
+hombre.add(new Dancer("Mariano", "h"))
 
-for(let x = 0; x < couple.length; x++)console.log("Pareja ",x+1," : ",couple[x])
+mujer.add(new Dancer("Xelhá","m"))
+mujer.add(new Dancer("Nahibi","m"))
+mujer.add(new Dancer("Margarita","m"))
+mujer.add(new Dancer("Zocorro","m"))
+
+dance(hombre,mujer)
+if (!mujer.vacio()) {
+    console.log(mujer.primero().name + " esta esperando.");
+}
+if (!hombre.vacio()) {
+    console.log('+--------------------------------+');
+    console.log('     '+hombre.primero().name + " está en espera.");
+    console.log('+--------------------------------+');
+}
