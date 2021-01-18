1c40b53622721f95bfd218b61607354d4825e783
carlosstc1 <ctene@ucol.mx>
2021-01-17 19:48:19 -0600

    Sacar filas

--- 2p/Matrices/Ej2.js
+++ 2p/Matrices/Ej2.js
@@ -6,7 +6,5 @@ let n = Math.floor(Math.random() * (4 - 0)) + 0;;
 console.log('Matríz Original');
 for (let i=0; i<Matriz.length; i++){
     console.log('['+(i+1)+']'+Matriz[i]);}
-console.log('Matríz sin la fila '+(n+1));
-Matriz.splice(n,1)
-for (let i=0; i<Matriz.length; i++){
-    console.log(Matriz[i]);}
+console.log('Línea  '+(n+1)+' de la matríz: ');
+console.log(Matriz[n]);
